4c56e3915bf52fa90516e2c63c4cbfed83555e70
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2022-04-23 00:00:24 +0300

    Migrating to gradle kts dsl

---
+++ client/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    application
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    implementation(project(":common"))
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("main.Main")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
